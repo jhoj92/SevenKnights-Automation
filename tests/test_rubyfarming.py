361812ee5ca1dd4e1b28dbd6f87ec865a0eefb61
jhoj92 <jhoj92@gmail.com>
2026-03-16 11:11:14 +0900

    오타 수정

--- tests/test_rubyfarming.py
+++ tests/test_rubyfarming.py
@@ -3,7 +3,7 @@ from pages.step_page import FarmingPage
 
 class TestSevenKnights:
     def test_ruby_farming(self):
-        """쫄작 자돟화 테스트"""
+        """쫄작 자동화 테스트"""
 
         tutorial = FarmingPage()
         tutorial.ruby_farming() 
